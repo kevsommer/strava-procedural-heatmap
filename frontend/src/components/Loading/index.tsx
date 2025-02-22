233acb7962f6fe9d09e6e0ef9974b820067d72ef
kevsommer <kevsommer96@gmail.com>
2025-02-22 12:07:57 +0100

    class to className

--- frontend/src/components/Loading/index.tsx
+++ frontend/src/components/Loading/index.tsx
@@ -1,7 +1,7 @@
 import { CircularProgress } from "@mui/material";
 
 const Loading = () => (
-  <div class="c-loading">
+  <div className="c-loading">
     <CircularProgress color="secondary" size={200} />
   </div>
 );
--- frontend/src/pages/Landing/index.tsx
+++ frontend/src/pages/Landing/index.tsx
@@ -3,13 +3,13 @@ import './Landing.css';
 
 const Landing = () => {
   return (
-    <div class="c-landing-page">
+    <div className="c-landing-page">
       <h1>Strava Heatmap Generator</h1>
       <p>
         This page can be used to generate a procedurally rendered heatmap from your Strava activities.
       </p>
       <RouterLink to="/code">
-        <button class="c-landing-page__button">
+        <button className="c-landing-page__button">
           Connect with Strava
         </button>
       </RouterLink>
