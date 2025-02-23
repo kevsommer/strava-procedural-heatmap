07f9f9d6e0ed9c1c87dc298b4e7d4f38e3e32f38
kevsommer <kevsommer96@gmail.com>
2025-02-23 21:04:51 +0100

    adds deploy script

---
+++ deploy.sh
@@ -0,0 +1,14 @@
+
+#!/bin/bash
+cd "$(dirname "$0")" || exit
+
+git fetch origin main
+LOCAL=$(git rev-parse HEAD)
+REMOTE=$(git rev-parse origin/main)
+
+if [ "$LOCAL" != "$REMOTE" ]; then
+    echo "Updates detected. Deploying changes..."
+    git pull origin main
+    docker-compose down
+    docker-compose build
+    docker-compose up -d
